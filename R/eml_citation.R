31c4094cd5af093f3f3e39dc10f05ce743b17e6d
Carl Boettiger <cboettig@gmail.com>
2013-07-07 14:42:16 -0700

    documentation updated

--- R/eml_citation.R
+++ R/eml_citation.R
@@ -1,11 +1,10 @@
 #' Return the citation information to an EML file.  
 #' 
-#' @aliases citation.eml
 #' @param doc an object of the class eml, as returned from `eml_read`, or the filename of an eml object 
 #' @details Can simply be called as `citation(doc)` after reading in a file
 #' with eml_read.  
 #' @seealso \code{\link{citation}}
-eml_citation <- function(doc){
+citation.eml <- function(doc){
   if(is.character(doc)){
     # doc <- eml_read(doc)
     doc <- xmlParse(doc)
@@ -20,7 +19,13 @@ eml_citation <- function(doc){
 }
 
 
-eml_R_citation <- function(packagename, ...){
+#' Create a citation node
+eml_citation <- function(type = c("article", "book", "generic")){
+
+}
+
+
+eml_R_citation <- function(r_citation, ...){
   eml_citation(...)
 }
 
