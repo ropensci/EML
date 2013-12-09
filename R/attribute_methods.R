2eb17a8aff71cf665d9083d7d3c4f38cb997d57c
Carl Boettiger <cboettig@gmail.com>
2013-12-09 11:30:51 -0800

    all tests passing (include ommitted seperate methods files)

---
+++ R/attribute_methods.R
@@ -0,0 +1,55 @@
+########################### Extraction methods ###################################
+## methods extract standard R formats (e.g. the metadata list format) 
+
+#' @include attribute.R
+
+setGeneric("extract", function(from, using) standardGeneric("extract"))
+
+
+setMethod("extract", signature("attributeList"), function(from){
+  lapply(from@attribute, 
+         function(x) 
+           list(x@attributeName,
+                x@attributeDefinition,
+                extract(x@measurementScale)))
+})
+
+ 
+setMethod("extract", signature("measurementScale"), function(from){
+# Find out which scale is not empty...
+  plyr::compact(lapply(slotNames(from), function(s)
+    if(!isEmpty(slot(from, s)))
+      extract(slot(from, s))))[[1]]
+})
+
+## FIXME write these methods! 
+setMethod("extract", signature("nominal"), function(from){
+  x <- from@nonNumericDomain
+  if(!isEmpty(x@enumeratedDomain)){  # Factor
+    code <- sapply(x@enumeratedDomain@codeDefinition, function(y) y@code)
+    def <- sapply(x@enumeratedDomain@codeDefinition, function(y) y@definition)
+    names(def) <- code
+    def 
+  } else if(!isEmpty(x@textDomain)) { # character
+    x@textDomain@definition
+  }
+})
+setMethod("extract", signature("ordinal"), function(from){
+  extract(as(from, "nominal"))
+})
+setMethod("extract", signature("ratio"), function(from){
+  if(!isEmpty(from@unit@standardUnit))
+    from@unit@standardUnit
+  else if(!isEmpty(from@unit@customUnit))
+    from@unit@customUnit
+})
+setMethod("extract", signature("interval"), function(from){
+  extract(as(from, "ratio"))
+})
+setMethod("extract", signature("dateTime"), function(from){
+  from@formatString
+})
+
+
+
+
---
+++ R/physical_methods.R
@@ -0,0 +1,91 @@
+#' @include physical.R
+
+########### extract methods for physical
+
+setMethod("extract", signature("physical", "character"),
+          function(from, using=NA){
+            # The "ordered" class does not have a coercion defined from character
+            # Better to just treat it as a factor 
+            using <- gsub("ordered", "factor", using) 
+            ## FIXME use the EML to determine read.csv options
+            dat <- read.csv(filepath(from))  #colClasses=using)
+          })
+# If col_classes(object) returns a list, means that colClasses are not simple types (e.g. c("ordered", "factor") or "NULL")
+# See read.csv for details.  Meanwhile, if we get a list, just read in ignoring the declared colClasses
+setMethod("extract", signature("physical", "list"),
+          function(from, using=NA){
+            ## FIXME use the EML to determine read.csv options
+            dat <- read.csv(filepath(from)) 
+          })
+
+
+
+## if no `using` option is defined: 
+setMethod("extract", signature("physical"),
+          function(from, using=NA){
+            ## FIXME use the EML to determine read.csv options
+            dat <- read.csv(filepath(from)) 
+          })
+
+
+
+
+#################################################################
+
+
+## FIXME  exported for test only, maybe not necessary?
+## extraction might not be the best way to define these.  
+
+
+setMethod("filepath", signature("physical"),
+          function(from){
+            path <- paste0(from@dirname, from@objectName)
+            if(file.exists(path))
+              path
+            else {
+              fail = download.file(from@distribution@online@url, 
+                                      destfile = path, method="auto")
+              if(!fail)
+                path
+              else
+                stop(paste("could not find file", from@objectName, "in", from@dirname, "or at", from@distribution@online@url))
+            }
+          })
+
+
+### Coerce data.frame to physical (csv) using constructor function:
+
+setAs("data.frame", "physical", function(from)
+      eml_physical(dat)) 
+
+
+####### Internal function to coerce data.frame to physical #######
+## Custom generator. Consider making int an S4 method?  
+eml_physical <- function(dat, filename=character(0), ...){
+  if(length(filename) == 0) 
+    filename = paste(uuid::UUIDgenerate(), ".csv", sep="")
+  write.csv(dat, file = filename, row.names=FALSE, ...)
+  new("physical", 
+      objectName = filename, 
+      size = as(object.size(dat), "numeric"), 
+      characterEncoding = guess_encoding(),
+      dataFormat = new("dataFormat", 
+                       textFormat = new("textFormat", 
+                                 numHeaderLines = 1L,
+                                 numFooterLines = 0L,
+                                 recordDelimiter = "\\r\\n",
+                                 attributeOrientation = "column",
+                                 simpleDelimited = 
+                                   new("simpleDelimited", fieldDelimiter = ",")))) 
+}
+# Attempt to determine encoding. Helper function
+guess_encoding <- function(){
+  encoding = getOption("encoding")
+  if(encoding == "native.enc")
+    encoding = tryCatch(Sys.getlocale("LC_CTYPE"), 
+                        error = function(e) character(0))
+  encoding
+}
+
+
+
