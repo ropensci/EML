627360b6a3ff767aa4ee1d26cb8ef7da83d6b79a
Carl Boettiger <cboettig@gmail.com>
2013-12-09 11:40:04 -0800

    merge the internal constructor functions for coverage sub-modules into coverage_methods.R

--- R/coverage_methods.R
+++ R/coverage_methods.R
@@ -89,5 +89,94 @@ setMethod("species", signature("taxonomicCoverage"),
 
 
 
+### Internal helper constructors 
+
+
+## Constructor for `taxonomicCoverage` class
+eml_taxonomicCoverage <- function(scientific_names){
+  if(is(scientific_names, "taxonomicCoverage"))
+    scientific_names
+  else 
+    new("taxonomicCoverage", 
+      taxonomicClassification = 
+      new("ListOftaxonomicClassification", 
+          lapply(scientific_names, as, "taxonomicClassification")))
+}
+
+#' @include resource.R
+
+## FIXME work with a variety of dateTime formats
+eml_temporalCoverage <- function(dates){
+  if(is(dates, "temporalCoverage"))
+    dates
+  else {
+    if(length(dates) == 2){
+    node <- new("temporalCoverage", 
+                rangeOfDates = 
+                new("rangeOfDates", 
+                     beginDate = 
+                     new("beginDate", 
+                         calendarDate = to_ISO8601(dates[1])),
+                     endDate = 
+                     new("endDate", 
+                         calendarDate = to_ISO8601(dates[2]))))
+   }
+   node
+  }
+}
+
+#' @include eml_yaml.R
+setMethod("show", signature("temporalCoverage"), function(object) show_yaml(object))
+
+
+
+# FIXME should be an S4 coersion method(?)
+to_ISO8601 <- function(x){
+  if(is.character(x)) ## Attempt to make most standard formats into the desired format  
+    x <- as.Date(x, '%Y-%m-%d')  
+  if(is(x, "Date")) # and then convert into this format. (Note this is not an  else if call!)
+    format(x, "%Y-%m-%d")
+}
+
+
+
+#' Helper constructor for creating geographicCoverage node
+#' 
+#' Implements only a subset of possible specifications for coverage
+#' @param description the text-based description of the geography.  This can also 
+#' be an`eml::geographicCoverage` object. 
+#' @param NSEWbox the North, South, East and West coordinates, in signed decimal degrees
+#' latitude/longitude, respectively.  Additionally, the minimum and maximum altitude in
+#' meters can optionally be given as the 5th and 6th values of this numeric vector.  This
+#' value can only be left as NULL if description is a completed `eml::geographicCoverage` 
+#' object.  
+eml_geographicCoverage <- function(description, NSEWbox=NULL){
+  if(is(description, "geographicCoverage"))
+    node <- description
+  else {
+    node <- new("geographicCoverage", 
+                geographicDescription = description,
+                boundingCoordinates = 
+                new("boundingCoordinates", 
+                    northBoundingCoordinate = NSEWbox[1],
+                    southBoundingCoordinate = NSEWbox[2],
+                    eastBoundingCoordinate =  NSEWbox[3],
+                    westBoundingCoordinate = NSEWbox[4]))
+    if(length(NSEWbox) == 6){ # include altitude min and max in meters
+      node@boundingCoordinates@boundingAltitudes <- 
+        new("boundingAltitudes", 
+            altitudeMinimum  = NSEWbox[5], 
+            altitudeMaximum  = NSEWbox[6], 
+            altitudeUnits = "meter")
+    }
+    node
+  }
+}
+
+
+#' @include eml_yaml.R
+setMethod("show", signature("geographicCoverage"), function(object) show_yaml(object))
+
+
 
 
