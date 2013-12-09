0535be075fdac0a9ec3b11a168b4ad74eb71722a
Carl Boettiger <cboettig@gmail.com>
2013-12-09 10:12:25 -0800

    maintain collate list separately

---
+++ R/collate.R
@@ -0,0 +1,9 @@
+#' 
+#' @include party.R 
+#' @include resource.R
+#' @include attribute.R
+#' @include coverage.R
+
+# Notes: 
+# - resource actually references coverage, and coverage references resource....  
+#  
