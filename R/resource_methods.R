de7c5d9c95cc46b8eef6f017ddcf9a846cf0f7ab
Carl Boettiger <cboettig@gmail.com>
2013-12-11 17:52:48 -0800

    method for generating keywordSet node from list

---
+++ R/resource_methods.R
@@ -0,0 +1,23 @@
+
+
+construct_keywordSet <- function(x){
+# expects x is a list of character vectors, named by keywordThesaurus
+  out <- new("ListOfkeywordSet", lapply(x, 
+    function(y){
+      new("keywordSet", 
+          keyword = new("ListOfkeyword", 
+                        lapply(y, function(z) 
+                               new("keyword", keyword = z))))
+  }))
+  thes <- names(x)
+  for(i in 1:length(thes)){
+    out[[i]]@keywordThesaurus <- thes[i]
+  }
+  out
+}
+
+
+#
+#setAs("list", "keywordSet", function(from){
+#
+#})
