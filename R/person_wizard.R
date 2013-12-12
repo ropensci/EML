4673643a1bcea7c2d810a2fe24ddaaa13f67470d
Carl Boettiger <cboettig@gmail.com>
2013-12-12 14:56:17 -0800

    use newlines

--- R/person_wizard.R
+++ R/person_wizard.R
@@ -1,17 +1,17 @@
 person_wizard <- function(type){
   p <- new(type)
-  p@individualName@givenName <- readline(paste("Given name for the", type))
-  p@individualName@surName <- readline("Surname?")
+  p@individualName@givenName <- readline(paste("Given name for the", type, "\n"))
+  p@individualName@surName <- readline("Surname?\n")
   if(type == "contact"){
-    p@electronicMailAddress <- readline("Email address for thecontact? (or leave blank and hit <Enter> provide a mailing address)")
+    p@electronicMailAddress <- readline("Email address for the contact?\n (or leave blank and hit <Enter> provide a mailing address)\n")
     p@address <-  new("address", 
-        deliveryPoint = readline("deilvery point (street address)?"),
+        deliveryPoint = readline("deilvery point (street address)?\n"),
         city = readline("city?"),
-        administrativeArea = readline("State or adminstrative area?"),
-        postalCode =  readline("zip code or postal code?"),
-        country = readline("country?"))
+        administrativeArea = readline("State or adminstrative area?\n"),
+        postalCode =  readline("zip code or postal code?\n"),
+        country = readline("country?\n"))
   } else if(type == "associatedParty"){
-    p@role <- readline("role of this associated party?")
+    p@role <- readline("role of this associated party?\n")
   }
   if(type != "contact")
     p <- c(p)
