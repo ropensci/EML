6e669018e192da8034017edd15033e2d1e5a33c6
Carl Boettiger <cboettig@gmail.com>
2013-12-11 17:35:45 -0800

    eml_read gets the namespaces from the file, rather than the default namespaces

--- R/eml_read.R
+++ R/eml_read.R
@@ -15,17 +15,24 @@ NULL
 #' @aliases eml_read read.eml
 eml_read <- function(file,  ...){
 
-  ## support for file name, XMLInternalDocument, URL, or identifier
+  ## Handle different input arguments: ## 
 
+  ## XMLDocument                      FIXME support XMLInternalElementNode! 
   if(is(file, "XMLInternalDocument")){  # if already parsed XML, e.g. from d1_get
     doc <- file
+
+  ## Local Path
   } else if(file.exists(file)){ # if is a local file path...
     doc <- xmlParse(file=file, ...)
+  
+  ## Remote path
   } else if(is.character(file)){ ## Assume a remote access method?
       require(httr)
+      ## URL 
       if(gsub("^(....).*", "\\1", file) %in% c("http", "ftp:", "file")) { ## Is it a URL?
-        doc <- content(GET(file), "parsed", "text/xml") # content type not detected automatically
-        # FIXME unlink the downloaded xml file after parsing? (may have to be after coercion to eml, due to XML external pointers)
+        doc <- content(GET(file), "parsed", "text/xml") 
+      
+      ## Identifier 
       } else {   ## Assume object is a DataONE identifer(?)
         doc <- content(GET(paste0("https://cn.dataone.org/cn/v1/object/", file)), "parsed", "text/xml") # content type actually is detected.
       }
@@ -33,9 +40,16 @@ eml_read <- function(file,  ...){
     stop("cannot find or parse file")
   }
 
+
   ## Parse
   root <- xmlRoot(doc)
   s4 <- as(root, "eml")
+
+  ## Update the namespaces 
+  s4@namespaces <- sapply(xmlNamespaceDefinitions(doc), `[[`, "uri")
+
+  ## Unlink external pointer (saves memory)  
+  free(doc)
 
   ##  Keep track of dirname in additional resource files (e.g. csv's) are available in same directory
   s4@dirname <- paste0(dirname(file), "/")
