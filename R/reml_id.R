14a67073c1c1c888fc9fb201c7630c0c6e8b8391
Carl Boettiger <cboettig@gmail.com>
2014-01-03 17:10:01 -0800

    set default scope as 'system', not 'document'

--- R/reml_id.R
+++ R/reml_id.R
@@ -6,7 +6,7 @@ reml_id <- function(prefix="reml_") {
       system <- "uuid"
     } else {
       id <- paste(prefix, format(Sys.time(), "%Y%m%d%H%M%s"), "1", sep=".")
-      scope <- "document"
+      scope <- "system"
       system <- "reml"
     }
     c(id = id, system = system, scope = scope)
