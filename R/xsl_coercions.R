6d09f7e003b988a4c75dbb7865be348016b2a2fb
Carl Boettiger <cboettig@gmail.com>
2014-01-20 21:46:09 -0800

    cleaning up

--- R/xsl_coercions.R
+++ R/xsl_coercions.R
@@ -9,5 +9,5 @@ get_rdf <- function(file){
   require(Sxslt)
   # FIXME detect EML version first in selecting which stylesheet to use.
   to_rdf <- system.file("xsl", "eml210toDublinCore.xsl", package="EML")
-  rdf <- xsltApplyStyleSheet(file, to_rdf)
+  rdf <- Sxslt::xsltApplyStyleSheet(file, to_rdf)
 }
