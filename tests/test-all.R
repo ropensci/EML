ac7993c0c83f4f093f1be8d19d3e450b0c3f3350
Carl Boettiger <cboettig@gmail.com>
2014-01-27 13:21:49 -0800

    handle old versions of testthat

--- tests/test-all.R
+++ tests/test-all.R
@@ -1,4 +1,7 @@
-
-library(testthat)
-#test_package("EML")
-test_check("EML")
+if (packageVersion("testthat") >= "0.7.1.99") {
+    library(testthat)
+  test_check("EML")
+} else {
+  library(testthat)
+  test_package("EML")
+}
