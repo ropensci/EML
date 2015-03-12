1482d9244d2ec69cacba3942053a7897b11fe29f
Matt Jones <gitcode@magisa.org>
2015-03-12 06:56:59 -0700

    Add support for otherEntity types.

--- R/dataset_classes.R
+++ R/dataset_classes.R
@@ -1,8 +1,9 @@
 #' @include reml_environment.R
-#' @include eml_config.R 
+#' @include eml_config.R
 #' @include resource_classes.R
 #' @include dataTable_classes.R
 #' @include methods_classes.R
+#' @include entity_classes.R
 
 # #' @include spatialRaster.R
 # #' @include spatialVector.R
@@ -10,48 +11,57 @@
 
 
 setClass("ListOfdataTable", contains = "list")
+setClass("ListOfotherEntity", contains = "list")
 
 #' concatenate
-#' 
+#'
 #' concatenate
 #' @param x,... dataTables to concatenate
 #' @param recursive Needed for compatibility with generic, otherwise ignored
 #' @rdname class-dataTable
 setMethod("c", signature("dataTable"), function(x, ..., recursive = FALSE) new("ListOfdataTable", list(x, ...)))
 
+#' concatenate
+#'
+#' concatenate
+#' @param x,... otherEntities to concatenate
+#' @param recursive Needed for compatibility with generic, otherwise ignored
+#' @rdname class-otherEntity
+setMethod("c", signature("otherEntity"), function(x, ..., recursive = FALSE) new("ListOfotherEntity", list(x, ...)))
 
 ## A Hack to get element order correct without writing
-## a custom S4Toeml function (which writes slots in 
+## a custom S4Toeml function (which writes slots in
 ## order provided by slotNames), since resourceGroup elements
 ## must come before these elements
-setClass("dataset_elements",     
-         slots = c(purpose = "character", 
+setClass("dataset_elements",
+         slots = c(purpose = "character",
                    contact = "contact",
                    publisher = "publisher",
                    methods = "methods",
 #                   spatialRaster = "ListOfspatialRaster",
 #                   spatialVector = "ListOfspatialVector",
 #                   view = "view",
-                   dataTable = "ListOfdataTable"), 
-         contains="id_scope") 
+                   dataTable = "ListOfdataTable",
+                   otherEntity = "ListOfotherEntity"),
+         contains="id_scope")
 
 ## Declaration of the dataset class
 ##
-## dataset_elements should really be in `representation`, but 
-## must come after resourceGroup elements se we define them as 
-## a separate class.  
+## dataset_elements should really be in `representation`, but
+## must come after resourceGroup elements se we define them as
+## a separate class.
 setClass("dataset",
-         contains = c("resourceGroup", 
-                      "dataset_elements", 
+         contains = c("resourceGroup",
+                      "dataset_elements",
                       "referencesGroup"),
-# if defining a prototype, undefined elements default to NULL?? WTF?  
-        prototype = prototype(coverage = new("coverage"), 
+# if defining a prototype, undefined elements default to NULL?? WTF?
+        prototype = prototype(coverage = new("coverage"),
                               pubDate = as.character(Sys.Date()),
-                              title = "metadata", 
-                              # FIXME Make title something more intelligent.  Use id?  
+                              title = "metadata",
+                              # FIXME Make title something more intelligent.  Use id?
                               intellectualRights = as.character(get("defaultRights", envir=EMLConfig))))
 
-## Coercion methods to/from XML 
+## Coercion methods to/from XML
 setAs("XMLInternalElementNode", "dataset",  function(from) emlToS4(from))
 setAs("dataset", "XMLInternalElementNode",   function(from) S4Toeml(from))
 
--- R/entity_classes.R
+++ R/entity_classes.R
@@ -1,7 +1,7 @@
 #' @include methods_classes.R
 #' @include coverage_classes.R
 #' @include physical_classes.R
-setClass("entityGroup", 
+setClass("entityGroup",
          slots = c(entityName = "character",
                    entityDescription = "character",
                    alternateIdentifier = "character",
@@ -13,3 +13,15 @@ setClass("entityGroup",
 setAs("XMLInternalElementNode", "entityGroup",  function(from) emlToS4(from))
 setAs("entityGroup", "XMLInternalElementNode",   function(from) S4Toeml(from))
 
+setClass("otherEntity_slots",
+         slots = c(attributeList = "attributeList",  ## Hack to make entityGroup slots listed first!
+                   entityType = "character"),
+         contains = "id_scope")
+
+setAs("XMLInternalElementNode", "otherEntity_slots",  function(from) emlToS4(from))
+setAs("otherEntity_slots", "XMLInternalElementNode",   function(from) S4Toeml(from))
+
+setClass("otherEntity",
+         contains=c("entityGroup", "otherEntity_slots"))
+setAs("XMLInternalElementNode", "otherEntity",  function(from) emlToS4(from))
+setAs("otherEntity", "XMLInternalElementNode",   function(from) S4Toeml(from))
