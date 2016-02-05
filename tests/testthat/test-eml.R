9be0de51207c70a43a62cb551ad3ef09a3a5a147
Carl Boettiger <cboettig@gmail.com>
2016-02-05 20:12:33 +0000

    tidy test here too

--- tests/testthat/test-eml.R
+++ tests/testthat/test-eml.R
@@ -8,19 +8,24 @@ testthat::test_that("We can parse the sample EML file correctly", {
   eml <- as(node, "eml")
   out <- as(eml, "XMLInternalElementNode")
 
+  sink("test.log")
   ## Ordering differs
   #names(xmlChildren(node[["dataset"]]))
   #names(xmlChildren(out[["dataset"]]))
 
+
   testthat::expect_identical(sort(names(xmlChildren(node[["dataset"]]))), sort(names(xmlChildren(out[["dataset"]]))))
 
   ## Nodes without repeated elements are identical (as text)
-  testthat::expect_identical(saveXML(node[["dataset"]][["purpose"]]), saveXML(out[["dataset"]][["purpose"]]))
+  testthat::expect_identical(show(node[["dataset"]][["purpose"]]), show(out[["dataset"]][["purpose"]]))
 
   ## Ordering differs
-  #testthat::expect_identical(saveXML(node[["dataset"]][["distribution"]]), saveXML(out[["dataset"]][["distribution"]]))
+  #testthat::expect_identical(show(node[["dataset"]][["distribution"]]), showL(out[["dataset"]][["distribution"]]))
 
   #out[["dataset"]][["distribution"]]
   #node[["dataset"]][["distribution"]]
+
+  sink()
+  unlink("test.log")
 })
 
