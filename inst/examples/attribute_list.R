faa0b0cea5eddc4186c1ad525f9a419c22d5e480
Carl Boettiger <cboettig@gmail.com>
2016-02-17 23:16:04 +0000

    Working on helper functions

--- inst/examples/attribute_list.R
+++ inst/examples/attribute_list.R
@@ -2,11 +2,28 @@
 library(purrr)
 
 getone <- function(x){
-  if(length(x) > 0)
-    x[[1]]@.Data
-  else
-    NA
+  if(is(x, "list")){
+    if(length(x) == 0){
+      cls <- sub("^ListOf", "", class(x))
+      x <- new(class(x), list(new(cls)))
+    }
+  }
+  x[[1]]
 }
+
+or_na <- function(x){
+  if(length(x) == 0)
+    x <- NA
+  x
+}
+
+get_reference <- function(n, eml){
+  id <- n@ReferencesGroup@references[[1]]
+  doc = XML::xmlParse(write_eml(eml))
+  node <- XML::xpathApply(doc, sprintf("//*[@id = '%s']", id))[[1]]
+  eml2:::emlToS4(node)
+}
+
 
 eml <- read_eml("inst/xsd/test/eml-datasetWithAttributelevelMethods.xml")
 A <- eml@dataset@dataTable[[1]]@attributeList@attribute
@@ -18,11 +35,41 @@ choice <- function(s4){
   who[!drop]
 }
 
+## Attribute name/def
 purrr::map_df(A, function(a)
   data.frame(name = a@attributeName,
-             label = getone(a@attributeLabel),      # optional, can repeat (though goodness knows why)
-             type = getone(a@storageType),          # optional, can repeat (though goodness knows why)
-             missing = getone(a@missingValueCode),  # optional, can actually be multiple
+             label = or_na(getone(a@attributeLabel)@.Data),      # optional, can repeat (though goodness knows why)
+             type = or_na(getone(a@storageType)@.Data),          # optional, can repeat (though goodness knows why)
+             missing = or_na(getone(a@missingValueCode)@.Data),  # optional, can actually be multiple
              scale = choice(a@measurementScale),
              def = a@attributeDefinition,
              stringsAsFactors = FALSE))
+
+## Ratio/Interval maps
+purrr::map_df(A, function(a){
+  name = a@attributeName
+  print(name)
+  scale = choice(a@measurementScale)
+  if(scale %in% c("ratio", "interval")){
+    b <- slot(a@measurementScale, scale)
+
+    ## Perform Reference lookup.  FIXME consider performing globally.
+    if(choice(b@numericDomain) == "ReferencesGroup")
+      b@numericDomain <- get_reference(b@numericDomain, eml)
+    else {
+      bounds <- getone(b@numericDomain@BoundsGroup@bounds)
+
+    data.frame(name = name,
+               unit = or_na(slot(b@unit, choice(b@unit))@.Data),
+               precision = or_na(b@precision@.Data),
+               type = or_na(b@numericDomain@numberType@.Data),
+               lower_bound = or_na(bounds@minimum@.Data),
+               upper_bound = or_na(bounds@maximum@.Data),
+      stringsAsFactors = FALSE)
+    }
+  } else {
+    NULL
+  }
+})
+
+
