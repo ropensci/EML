39a1b9dd68849a9b1f377fad21a96933047ea3bb
Carl Boettiger <cboettig@gmail.com>
2016-02-26 21:25:38 +0000

    set_attributes can take attribute data.frames without all 17 metadata columns.

--- R/attributes.R
+++ R/attributes.R
@@ -257,6 +257,9 @@ choice <- function(s4){
 #' @param attributes a joined table of all attribute metdata
 #' (such as returned by \code{\link{get_attributes}}, see details)
 #' @param factors a table with factor code-definition pairs; see details
+#' @param col_classes optional, list of R column classes (numeric, factor, date, character)
+#' will let the function infer missing 'domain' and 'measurementScale' values for attributes column.
+#' Should be in same order as attributeNames in the attribute table, or be a named list.
 #' @details The attributes data frame must use only the recognized column
 #' headers shown here.  The attributes data frame must contain columns for required metadata.
 #' These are:
@@ -280,18 +283,34 @@ choice <- function(s4){
 #'
 #' @return an eml "attributeList" object
 #' @export
-set_attributes <- function(attributes, factors){
+set_attributes <- function(attributes, factors, col_classses = NULL){
 
-  ##  check attributes data.frame.  must declare required columns: attributeName
-  ## consider inferring "domain" & "measurementScale" given optional column classes?
-  ## Add NA columns if necessary?
-  ## coerce all scolumns to character values!
+  ##  check attributes data.frame.  must declare required columns: attributeName, (attributeDescription, ....)
+  if(! "attributeName" %in% names(attributes))
+    stop("attributes table must include an 'attributeName' column")
+
+  ## infer "domain" & "measurementScale" given optional column classes
+  if(!is.null(col_classses))
+    attributes <- merge(attributes, infer_domain_scale(col_classes, attributes$attributeName), all = TRUE)
+  ## Add NA columns if necessary FIXME some of these can be missing if their class isn't represented, but otherwise must be present
+  for(x in c("precision", "minimum", "maximum", "unit", "numberType", "formatString", "definition",
+                         "pattern", "source", "attributeLabel", "storageType", "missingValueCode",
+                         "missingValueCodeExplanation")){
+    attributes <- add_na_column(x, attributes)
+  }
 
   out <- new("attributeList")
   out@attribute <- as(lapply(1:dim(attributes)[1], function(i)
     set_attribute(attributes[i,], factors = factors)), "ListOfattribute")
   out
 }
+
+add_na_column <- function(column, df){
+  if(! column %in% names(df))
+    df$column <- NA
+  df
+}
+
 
 na2empty <- function(x){
   if(!is.null(x))
@@ -388,3 +407,30 @@ is_customUnit <- function(x){
   standard_unit_list <- read.csv(system.file("units/standard_unit_list.csv", package = "eml2"))
   !(x %in% standard_unit_list[[1]])
 }
+
+
+
+infer_domain_scale <- function(col_classes, attributeName = names(col_classes)){
+  domain = col_classes
+  measurementScale = col_classes
+  domain[col_classes == "numeric"] <- "numericDomain"
+  domain[col_classes == "character"] <- "textDomain"
+  domain[col_classes %in% c("factor", "ordered")] <- "enumeratedDomain"
+  domain[col_classes %in% c("Date")] <- "dateTimeDomain"
+
+  measurementScale[col_classes == "numeric"] <- "ratio" # !
+  measurementScale[col_classes == "character"] <- "nominal"
+  measurementScale[col_classes == "ordered"] <- "ordinal"
+  measurementScale[col_classes == "factor"] <- "nominal"
+  measurementScale[col_classes %in% c("Date")] <- "dateTime"
+
+  ## storage type is optional, maybe better not to set this?
+  storageType[col_classes == "numeric"] <- "float"
+  storageType[col_classes == "character"] <- "string"
+  storageType[col_classes %in% c("factor", "ordered")] <- "string"
+  storageType[col_classes %in% c("Date")] <- "date"
+
+
+  data.frame(attributeName = attributeName, domain = domain, measurementScale = measurementScale, storageType = storageType)
+}
+
--- inst/examples/hf-205.R
+++ inst/examples/hf-205.R
@@ -38,3 +38,87 @@ description <- "The Geographic region of the kelp bed data extends along the Cal
 box <- c(-122.44, -117.15, 37.38, 30.00)
 
 eml2:::set_geographicCoverage(box, description)
+
+
+eml2:::set_taxonomicCoverage("Sarracenia purpurea")
+
+col.defs <- c("run.num" = "which run number (=block). Range: 1 - 6. (integer)",
+              "year" = "year, 2012",
+              "day" = "Julian day. Range: 170 - 209.",
+              "hour.min" = "hour and minute of observation. Range 1 - 2400 (integer)",
+              "i.flag" =  "is variable Real, Interpolated or Bad (character/factor)",
+              "variable" = "what variable being measured in what treatment (character/factor).",
+              "value.i" = "value of measured variable for run.num on year/day/hour.min.")
+
+unit.defs = list("which run number",
+                 "YYYY",
+                 "DDD",
+                 "hhmm")
+
+
+## attributes table
+
+attributes <-
+  dplyr::frame_data(
+    ~attributeName,  ~formatString, ~definition,         ~attributeDefinition,
+    "run.num",        NA,           "which run number", "which run number (=block). Range: 1 - 6. (integer)",
+    "year",          "YYYY",        NA,                 "year, 2012",
+    "day",           "DDD",         NA,                 "Julian day. Range: 170 - 209.",
+    "hour.min",      "hhmm",        NA,                 "hour and minute of observation. Range 1 - 2400 (integer)",
+    "i.flag",        NA,            NA,                 "is variable Real, Interpolated or Bad (character/factor)",
+    "variable",      NA,            NA,                 "what variable being measured in what treatment (character/factor).",
+    "value.i",       NA,            NA,                 "value of measured variable for run.num on year/day/hour.min."
+    )
+
+
+## Factors table:
+
+## Named char vectors are a natural way to define code/definition pairs in Code.
+i.flag <- c(R = "real",
+            I = "interpolated",
+            B = "bad")
+variable <- c(
+  control  = "no prey added",
+  low      = "0.125 mg prey added ml-1 d-1",
+  med.low  = "0,25 mg prey added ml-1 d-1",
+  med.high = "0.5 mg prey added ml-1 d-1",
+  high     = "1.0 mg prey added ml-1 d-1",
+  air.temp = "air temperature measured just above all plants (1 thermocouple)",
+  water.temp = "water temperature measured within each pitcher",
+  par       = "photosynthetic active radiation (PAR) measured just above all plants (1 sensor)"
+)
+
+value.i <- c(
+  control  = "% dissolved oxygen",
+  low      = "% dissolved oxygen",
+  med.low  = "% dissolved oxygen",
+  med.high = "% dissolved oxygen",
+  high     = "% dissolved oxygen",
+  air.temp = "degrees C",
+  water.temp = "degrees C",
+  par      = "micromoles m-1 s-1"
+)
+
+## Write these into a data.frame, because that's a more natural R type
+factors <- rbind(
+data.frame(
+  attributeName = "i.flag",
+  code = names(i.flag),
+  definition = unname(i.flag)
+),
+data.frame(
+  attributeName = "variable",
+  code = names(variable),
+  definition = unname(variable)
+),
+data.frame(
+  attributeName = "value.i",
+  code = names(value.i),
+  definition = unname(value.i)
+)
+)
+
+
+
+
+
