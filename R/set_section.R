1da8a85d172a455456403c261e3dc2d843690d35
Carl Boettiger <cboettig@gmail.com>
2016-03-09 05:01:23 +0000

    avoid hard dependency on rmarkdown

--- R/set_section.R
+++ R/set_section.R
@@ -2,8 +2,7 @@
 #' set_section
 #' 
 #' For any EML element that takes a "section" argument, this function can be used to generate the appropriate EML from a markdown-formatted file.
-#' @param file path to any document recognized by pandoc, such as Word .docx file or markdown file.
-#' @import rmarkdown
+#' @param file path to a file providing formatted input text, see details.
 #' @import XML
 #' @return a ListOfsection object that can be used in constructing any element with section slots, see examples
 #' @examples 
@@ -16,20 +15,36 @@
 #' set_section(f)
 #' new("description", section = set_section(f))
 #' 
-#' @details See http://rmarkdown.rstudio.com and http://pandoc.org for more details about coversions with pandoc & rmarkdown.  
-#' Pandoc comes pre-installed in RStudio
+#' @importFrom tools file_ext
+#' @details If the `rmarkdown` package is installed, then the input file can 
+#' be a Microsoft Word (.docx) file, a markdown file, or other file
+#' recognized by Pandoc (see http://pandoc.org), which will automate the conversion
+#' to a docbook. Otherwise, the input file should already be in docbook format (with 
+#' .xml or .dbk extension).  Note that pandoc comes pre-installed in RStudio and is 
+#' required for the rmarkdown package.  
 #' @export
 set_section <- function(file = NULL){
   
-  wd <- getwd()
-  dir <- tempdir()
-  file.copy(file, paste(dir, basename(file), sep="/"))
-  setwd(dir)
-  docbook_file = tempfile(tmpdir = ".", fileext = ".xml")
-  rmarkdown::pandoc_convert(basename(file), to = "docbook", output = docbook_file, options = "-s")
-  #rmarkdown::pandoc_convert(basename(file), to = "docbook", output = docbook_file, wd = dirname(file)) 
-  docbook <- XML::xmlParse(docbook_file)
-  setwd(wd)
+  if(!tools::file_ext(file) %in% c("xml", "dbk")){
+    
+    if (!requireNamespace("rmarkdown", quietly = TRUE)){
+      stop("rmarkdown package required to convert to Docbook format", call. = FALSE)
+    }
+    pandoc_convert <- getExportedValue("rmarkdown", "pandoc_convert")
+
+    wd <- getwd()
+    dir <- tempdir()
+    file.copy(file, paste(dir, basename(file), sep="/"))
+    setwd(dir)
+    docbook_file = tempfile(tmpdir = ".", fileext = ".xml")
+    pandoc_convert(basename(file), to = "docbook", output = docbook_file, options = "-s")
+    docbook <- XML::xmlParse(docbook_file)
+    setwd(wd)
+  
+  } else { 
+    docbook  <- XML::xmlParse(file)
+  }
+  ##
   
   sections <- XML::xpathApply(docbook, "/article/sect1", XML::xmlChildren)
   s <- lapply(sections, function(x) new("section", as(x, "list")))
