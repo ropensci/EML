9c26dd70b8e2faa422c10b0c9d90641e4fc71253
Bryce Mecum <mecum@nceas.ucsb.edu>
2016-05-06 07:36:53 -0800

    Move validation test files under inst/examples

--- tests/testthat/test-validation.R
+++ tests/testthat/test-validation.R
@@ -3,7 +3,7 @@ testthat::context("Validating EML documents")
 testthat::test_that("We return TRUE when validating valid documents", {
   library("XML")
   
-  f <- system.file("tests/testthat/", "example-eml-valid.xml", package = "EML")
+  f <- system.file("examples", "example-eml-valid.xml", package = "EML")
 
   testthat::expect_true(eml_validate(f))
   testthat::expect_message(eml_validate(f), NA)
@@ -12,7 +12,7 @@ testthat::test_that("We return TRUE when validating valid documents", {
 testthat::test_that("We return FALSE and messages when validating invalid documents", {
   library("XML")
   
-  f <- system.file("tests/testthat/", "example-eml-invalid.xml", package = "EML")
+  f <- system.file("examples", "example-eml-invalid.xml", package = "EML")
 
   testthat::expect_false(eml_validate(f))
   testthat::expect_message(eml_validate(f))
