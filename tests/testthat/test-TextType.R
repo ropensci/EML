cabef03f09a5a473c3b5bb0fb81b53ae89a4c08f
Carl Boettiger <cboettig@gmail.com>
2016-06-20 22:07:44 +0000

    tidy up tests

--- tests/testthat/test-TextType.R
+++ tests/testthat/test-TextType.R
@@ -1,20 +1,37 @@
+testthat::context("TextType")
+
+testthat::test_that("we can create TextType from a simple character string", {
+
 ## using a simple character string
 a <- set_TextType(text = "This is the abstract")
-as(a, "abstract")
+ab <- as(a, "abstract")
+testthat::expect_is(ab, "abstract")
+
+})
 
 ## Using an external markdown file
+testthat::test_that("we can create TextType using an external markdown file", {
+  
 f <- system.file("examples/hf205-abstract.md", package = "EML")
 a <- set_TextType(f)
-as(a, "abstract")
+ab <- as(a, "abstract")
+testthat::expect_is(ab, "abstract")
+})
 
-## Can also import from methods written in a .docx MS Word file.
+testthat::test_that("we can create TextType in a .docx MS Word file.", {
 f <- system.file("examples/hf205-abstract.docx", package = "EML")
 a <- set_TextType(f)
-as(a, "abstract")
+ab <- as(a, "abstract")
+testthat::expect_is(ab, "abstract")
+})
 
-## Documents can have richer formatting.  Documents with title headings use `section` instead of `para` notation
+testthat::test_that("Documents can have richer formatting.  Documents with title headings use `section` instead of `para` notation", {
 f <- system.file("examples/hf205-methods.docx", package = "EML")
 d <- set_TextType(f)
 node <- as(d, "description")
- 
-get_TextType(node, view = FALSE)
+testthat::expect_is("description")
+
+
+get_TextType(node, view = FALSE, output = tempfile())
+})
+
