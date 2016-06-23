2d01f05e43c5307e87ece81496cb7a694d49f3fb
masalmon <maelle.salmon@yahoo.se>
2016-06-23 13:00:41 +0200

    adds a test for set_coverage

--- R/set_coverage.R
+++ R/set_coverage.R
@@ -22,7 +22,23 @@
 #' @export
 #'
 #' @examples
-#' 
+#' coverage <- 
+#' set_coverage(begin = '2012-06-01', end = '2013-12-31',
+#'              sci_names = "Sarracenia purpurea",
+#'              geographicDescription = geographicDescription,
+#'              west = -122.44, east = -117.15, 
+#'              north = 37.38, south = 30.00,
+#'              altitudeMin = 160, altitudeMaximum = 330,
+#'             altitudeUnits = "meter")
+#' geographicDescription <- "The Geographic region of the kelp bed data
+#'  extends along the California coast, down through 
+#'  the coast of Baja, Mexico: Central California
+#'   (Halfmoon Bay to Purisima Point), 
+#'   Southern California (Point Arguello to 
+#'   the United States/Mexico border including the 
+#'   Channel Islands) and Baja California (points south
+#'    of the United States/Mexico border including several 
+#'    offshore islands)"
 #'coverage <- 
 #'  set_coverage(begin = '2012-06-01', end = '2013-12-31',
 #'               sci_names = "Sarracenia purpurea",
