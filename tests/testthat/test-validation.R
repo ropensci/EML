c766f232dff850b68737f27770ca9a1129f35d82
masalmon <maelle.salmon@yahoo.se>
2016-06-23 10:38:36 +0200

    adds test for validation of eml object not file

--- tests/testthat/test-validation.R
+++ tests/testthat/test-validation.R
@@ -15,6 +15,21 @@ testthat::test_that("We return TRUE when validating valid documents", {
   testthat::expect_message(eml_validate(f2, encoding = "latin1"), NA)
 })
 
+testthat::test_that("We return TRUE when validating valid eml objects", {
+  library("XML")
+  
+  f <- system.file("examples", "example-eml-valid.xml", package = "EML")
+  eml <- read_eml(f)
+  testthat::expect_true(eml_validate(eml))
+  testthat::expect_message(eml_validate(eml), NA)
+  
+  
+  f2 <- system.file("examples", "example-eml-valid-special-characters.xml", package = "EML")
+  eml2 <- read_eml(f2)
+  testthat::expect_true(eml_validate(eml2, encoding = "latin1"))
+  testthat::expect_message(eml_validate(eml2, encoding = "latin1"), NA)
+})
+
 testthat::test_that("We return FALSE and messages when validating invalid documents", {
   library("XML")
   
