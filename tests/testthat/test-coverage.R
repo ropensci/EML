21938ad8906ff65c286729d850ba3203e49707e8
Carl Boettiger <cboettig@gmail.com>
2016-10-13 20:40:18 +0000

    avoid downloading test file over https, use local file

--- tests/testthat/test-coverage.R
+++ tests/testthat/test-coverage.R
@@ -1,62 +1,62 @@
 testthat::context("Creating coverage")
 testthat::test_that("set_coverage creates a coverage object",{
   geographicDescription <- "The Geographic region of the kelp bed data extends along the California coast, down through the coast of Baja, Mexico: Central California (Halfmoon Bay to Purisima Point), Southern California (Point Arguello to the United States/Mexico border including the Channel Islands) and Baja California (points south of the United States/Mexico border including several offshore islands)"
-  
-  coverage <- 
+
+  coverage <-
     set_coverage(begin = '2012-06-01', end = '2013-12-31',
                  sci_names = "Sarracenia purpurea",
                  geographicDescription = geographicDescription,
-                 west = -122.44, east = -117.15, 
+                 west = -122.44, east = -117.15,
                  north = 37.38, south = 30.00,
                  altitudeMin = 160, altitudeMaximum = 330,
                  altitudeUnits = "meter")
-  
+
   testthat::expect_is(coverage, "coverage")
-  
-  
-  coverage <- 
+
+
+  coverage <-
     set_coverage(date = as.character(seq(from = as.Date("1970-01-01"), length = 56, by = "1 week")),
                  sci_names = "Sarracenia purpurea",
                  geographicDescription = geographicDescription,
-                 west = -122.44, east = -117.15, 
+                 west = -122.44, east = -117.15,
                  north = 37.38, south = 30.00,
                  altitudeMin = 160, altitudeMaximum = 330,
                  altitudeUnits = "meter")
-  
+
   testthat::expect_is(coverage, "coverage")
-  
-  coverage <- 
+
+  coverage <-
     set_coverage(begin = '2012-06-01', end = '2013-12-31',
                  sci_names = list(KINGDOM="Plantae", PHYLUM="Phaeophyta", CLASS="Phaeophyceae",
                                   ORDER="Laminariales",FAMILY="Lessoniaceae",GENUS="Macrocystis",
                                   genusSpecies="Macrocystis pyrifera",commonName="MAPY"),
                  geographicDescription = "California coast, down through Baja, Mexico",
-                 west = -122.44, east = -117.15, 
+                 west = -122.44, east = -117.15,
                  north = 37.38, south = 30.00)
-  
+
   testthat::expect_is(coverage, "coverage")
-  
-  coverage <- 
+
+  coverage <-
     set_coverage(begin = '2012-06-01', end = '2013-12-31',
                  sci_names = data.frame(KINGDOM="Plantae", PHYLUM="Phaeophyta", CLASS="Phaeophyceae",
                                         ORDER="Laminariales",FAMILY="Lessoniaceae",GENUS="Macrocystis",
                                         genusSpecies="Macrocystis pyrifera",commonName="MAPY"),
                  geographicDescription = "California coast, down through Baja, Mexico",
-                 west = -122.44, east = -117.15, 
+                 west = -122.44, east = -117.15,
                  north = 37.38, south = 30.00)
-  
+
   testthat::expect_is(coverage, "coverage")
   })
 
 testthat::test_that("set_taxonomicCoverage creates a taxonomicCoverage object",{
   # input type: data frame
-  taxon = set_taxonomicCoverage(data.frame(KINGDOM="Plantae", PHYLUM="Phaeophyta", 
+  taxon = set_taxonomicCoverage(data.frame(KINGDOM="Plantae", PHYLUM="Phaeophyta",
                                    CLASS="Phaeophyceae",ORDER="Laminariales",
                                    FAMILY="Lessoniaceae",GENUS="Macrocystis",
                                   genusSpecies="Macrocystis pyrifera",commonName="MAPY"))
   testthat::expect_is(taxon, "taxonomicCoverage")
   # input type: list
-  taxon = set_taxonomicCoverage(list(KINGDOM="Plantae", PHYLUM="Phaeophyta", 
+  taxon = set_taxonomicCoverage(list(KINGDOM="Plantae", PHYLUM="Phaeophyta",
                                            CLASS="Phaeophyceae",ORDER="Laminariales",
                                            FAMILY="Lessoniaceae",GENUS="Macrocystis",
                                            genusSpecies="Macrocystis pyrifera",commonName="MAPY"))
@@ -65,7 +65,7 @@ testthat::test_that("set_taxonomicCoverage creates a taxonomicCoverage object",{
 
 testthat::test_that("set_taxonomicCoverage create a object with correct depth", {
   # regular case
-  dt = data.frame(KINGDOM="Plantae", PHYLUM="Phaeophyta", 
+  dt = data.frame(KINGDOM="Plantae", PHYLUM="Phaeophyta",
                   CLASS="Phaeophyceae",ORDER="Laminariales",
                   FAMILY="Lessoniaceae",GENUS="Macrocystis",
                   genusSpecies="Macrocystis pyrifera",commonName="MAPY")
@@ -80,43 +80,41 @@ testthat::test_that("set_taxonomicCoverage create a object with correct depth", 
 
 testthat::test_that("set_taxonomicCoverage create valid EML",{
   testthat::skip_on_cran()
-  testthat::skip_on_travis()
-  
-  f = tempfile()
-  download.file("https://knb.ecoinformatics.org/knb/d1/mn/v2/object/doi%3A10.5063%2FF1BZ63Z8", f)
+
+  f <- system.file("examples", "coverage_test.xml", package = "EML")
   eml <- read_eml(f)
 
-  physical = eml_get(eml, "physical")
-  attributeList = eml_get(eml, "attributeList")
-  abstract = eml_get(eml, "abstract")
-  rights = eml_get(eml, "intellectualRights")
-  methods = eml_get(eml, "methods")
-  keys = eml_get(eml, "keywordSet")
+  physical <- eml_get(eml, "physical")
+  attributeList <- eml_get(eml, "attributeList")
+  abstract <- eml_get(eml, "abstract")
+  rights <- eml_get(eml, "intellectualRights")
+  methods <- eml_get(eml, "methods")
+  keys <- eml_get(eml, "keywordSet")
 
-  dataTable = new("dataTable",
+  dataTable <- new("dataTable",
                   entityName = "data.csv",
                   entityDescription = "test",
                   physical = physical[[1]][[1]],
                   attributeList = attributeList[[1]][[1]])
-  title = "test"
-  pubDate = "2000"
-  creator=new("ListOfcreator")
-  creator[[1]]=as(as.person("John Smith [cre]"), "creator")
+  title <- "test"
+  pubDate <- "2000"
+  creator <- new("ListOfcreator")
+  creator[[1]] <- as(as.person("John Smith [cre]"), "creator")
 
-  address = new("address",
+  address <- new("address",
                   deliveryPoint="123 Main St.",
                   city = "None",
                   administrativeArea = "None",
                   postalCode = "None",
                   country = "USA")
-  publisher = new("publisher", organizationName = "None", address = address)
-  contact = new("contact", individualName = creator[[1]]@individualName,
+  publisher <- new("publisher", organizationName = "None", address = address)
+  contact <- new("contact", individualName = creator[[1]]@individualName,
                   electronicMail = "None",
                   address = address,
                   organizationName = "None",
                   phone = "None")
 
-  df = data.frame(KINGDOM="Plantae", PHYLUM="Phaeophyta",
+  df <- data.frame(KINGDOM="Plantae", PHYLUM="Phaeophyta",
                      CLASS="Phaeophyceae",ORDER="Laminariales",
                      FAMILY="Lessoniaceae",GENUS="Macrocystis",
                      genusSpecies="Macrocystis pyrifera",commonName="MAPY")
@@ -127,7 +125,7 @@ testthat::test_that("set_taxonomicCoverage create valid EML",{
                  west = 0, east = 0,
                  north = 0, south = 0)
 
-  dataset = new("dataset",
+  dataset <- new("dataset",
                 title = title,
                 creator = creator,
                 contact = contact,
@@ -140,7 +138,7 @@ testthat::test_that("set_taxonomicCoverage create valid EML",{
                 methods = methods,
                 dataTable = c(dataTable))
   #create eml object
-  eml = new("eml",
+  eml <- new("eml",
             packageId = uuid::UUIDgenerate(),
             system = "uuid",
             dataset = dataset)
@@ -152,12 +150,12 @@ testthat::test_that("get_taxonomicCoverage", {
   f <- system.file("xsd/test/eml-i18n.xml", package="EML")
   eml <- read_eml(f)
   df <- EML:::get_taxonomicCoverage(eml@dataset@coverage@taxonomicCoverage)
-  
+
   testthat::expect_is(df, "data.frame")
   testthat::expect_equal(ncol(df), 8)
   testthat::expect_equal(nrow(df), 1)
-  
-  
+
+
   # coverage <- eml_get(eml, "coverage")
-  
+
   })
--- tests/testthat/test-eml_get.R
+++ tests/testthat/test-eml_get.R
@@ -1,12 +1,11 @@
 testthat::context("eml_find and eml_get")
 
-f = tempfile()
-download.file("https://knb.ecoinformatics.org/knb/d1/mn/v2/object/doi%3A10.5063%2FF1BZ63Z8",f)
+f <- system.file("examples", "coverage_test.xml", package = "EML")
 eml = read_eml(f)
 
 testthat::test_that("we can find and get an element", {
 
-  m <- eml_get(eml, "methods") 
+  m <- eml_get(eml, "methods")
   testthat::expect_is(m, "methods")
   m <- eml_find(eml, "methods") # ok
   testthat::expect_is(m, "methods")
@@ -15,16 +14,16 @@ testthat::test_that("we can find and get an element", {
 
 
 testthat::test_that("we can get child elements from arbitrary nodes", {
-  
+
   geographicCoverage <- eml_get(eml, "geographicCoverage") #works
-  
+
   # Note, this returns a list of all ListOfgeographicCoverage nodes...
   testthat::expect_is(geographicCoverage[[1]][[1]], "geographicCoverage")
-  
+
   ## How about an individual character-type element?
   x <- eml_get(geographicCoverage, "geographicDescription")
   testthat::expect_is(x, "character")
-  
+
   ## The fast way?
   eml_get(eml, "geographicDescription")  #return NULL
 
