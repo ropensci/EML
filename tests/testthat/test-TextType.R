a7f81dcbde43cf5f5024cead3bdfa64c769d32ad
Carl Boettiger <cboettig@gmail.com>
2016-10-13 21:45:05 +0000

    skip offending tests on windows

--- tests/testthat/test-TextType.R
+++ tests/testthat/test-TextType.R
@@ -11,27 +11,42 @@ testthat::expect_is(ab, "abstract")
 
 ## Using an external markdown file
 testthat::test_that("we can create TextType using an external markdown file", {
-  
-f <- system.file("examples/hf205-abstract.md", package = "EML")
-a <- set_TextType(f)
-ab <- as(a, "abstract")
-testthat::expect_is(ab, "abstract")
+
+  skip_on_cran()
+  skip_on_appveyor()
+  skip_on_os("windows")
+
+  f <- system.file("examples/hf205-abstract.md", package = "EML")
+  a <- set_TextType(f)
+  ab <- as(a, "abstract")
+  testthat::expect_is(ab, "abstract")
 })
 
 testthat::test_that("we can create TextType in a .docx MS Word file.", {
-f <- system.file("examples/hf205-abstract.docx", package = "EML")
-a <- set_TextType(f)
-ab <- as(a, "abstract")
-testthat::expect_is(ab, "abstract")
+  skip_on_cran()
+  skip_on_appveyor()
+  skip_on_os("windows")
+
+  f <- system.file("examples/hf205-abstract.docx", package = "EML")
+  a <- set_TextType(f)
+  ab <- as(a, "abstract")
+  testthat::expect_is(ab, "abstract")
 })
 
 testthat::test_that("Documents can have richer formatting.  Documents with title headings use `section` instead of `para` notation", {
-f <- system.file("examples/hf205-methods.docx", package = "EML")
-d <- set_TextType(f)
-node <- as(d, "description")
-testthat::expect_is(node, "description")
+
+  skip_on_cran()
+  skip_on_appveyor()
+  skip_on_os("windows")
 
 
-get_TextType(node, view = FALSE, output = tempfile())
+  f <- system.file("examples/hf205-methods.docx", package = "EML")
+  d <- set_TextType(f)
+  node <- as(d, "description")
+  testthat::expect_is(node, "description")
+
+  out <- tempfile()
+  get_TextType(node, view = FALSE, output = out)
+  unlink(out)
 })
 
