d71849a50622675f6cf9a32f32cdd2b61db1b1ea
Bryce Mecum <petridish@gmail.com>
2017-02-08 14:02:04 -0900

    Implement get_abstract and abstract part of get_TextType
    As discussed in #194, we decided to implement a helper function called
    get_abstract that would return a text representation of the EML
    abstract. My first attempt involved copying most of the function body
    of get_TextType into get_abstract so I refactored the two functions to
    make use of a node_to_file helper method that does the conversion with
    pandoc.

--- R/get_TextType.R
+++ R/get_TextType.R
@@ -15,15 +15,15 @@
 #'
 #' @examples
 #' \donttest{
-#' 
+#'
 #' ## Convert an EML abstract to markdown
 #' f <- system.file("examples/hf205.xml", package = "EML")
 #' eml <- read_eml(f)
-#' abstract <- eml_get(eml, "abstract") 
+#' abstract <- eml_get(eml, "abstract")
 #' get_TextType(abstract[[1]], "markdown", "abstract.markdown")
 #' readLines("abstract.markdown")
 #' unlink("abstract.markdown") # tidy up
-#' 
+#'
 #' ## Turn a docx file into EML abstract and preview at HTML
 #' f <- system.file("examples/hf205-abstract.docx", package = "EML")
 #' a <- as(set_TextType(f), "abstract")
@@ -35,13 +35,31 @@ get_TextType <-
            to = "html",
            output = tempfile(class(node), fileext = paste0(".", to)),
            view = TRUE) {
-    
+
+    node_to_file(node, to, output)
+
+    if (view && to == "html")
+      utils::browseURL(basename(output))
+  }
+
+#' Converts a node into a file by running it through Pandoc
+#'
+#' @param node any node that supports a docbook-like structure (abstract, methods, etc)
+#' @param to desired format, default is html, but can be any type supported by pandoc (docx, md, etc)
+#' @param output name of the desired output file
+#'
+#' @return creates a file requested.
+#' @export
+#'
+#' @examples
+node_to_file <-
+  function(node, to = "html", output = tempfile(class(node), fileext = paste0(".", to))) {
     ## eml_get returns a list of nodes always.
-    if(!isS4(node) && length(node) == 1) 
+    if(!isS4(node) && length(node) == 1)
       node <- node[[1]]
-    
+
     # serialize sections in ListOfsection or paras from ListOfpara into XML document, save, rmarkdown into desired format
-    x <- XML::xmlChildren(S4Toeml(node))
+    x <- XML::xmlChildren(EML:::S4Toeml(node))
 
     if (!requireNamespace("rmarkdown", quietly = TRUE)) {
       stop("rmarkdown package required to convert to Docbook format",
@@ -50,34 +68,82 @@ get_TextType <-
     pandoc_convert <-
       getExportedValue("rmarkdown", "pandoc_convert")
 
-    wd <- getwd()
-    dir <- tempdir()
-    file.copy(output, file.path(dir, basename(output)), overwrite = TRUE)
-    setwd(dir)
-    docbook_file <- tempfile(pattern = "docbook", tmpdir = ".", fileext = ".db")
-    
+    docbook_path <- tempfile(pattern = "docbook", fileext = ".db")
     doctype <- XML::Doctype(name = "section",
-                       public = c("-//OASIS//DTD DocBook XML V4.2//EN"),
-                       system = "http://oasis-open.org/docbook/xml/4.5/docbookx.dtd")
+                            public = c("-//OASIS//DTD DocBook XML V4.2//EN"),
+                            system = "http://oasis-open.org/docbook/xml/4.5/docbookx.dtd")
     y <- XML::newXMLNode("article", x)
-    XML::saveXML(XML::xmlDoc(y), 
-                 docbook_file, doctype = doctype)
+    XML::saveXML(XML::xmlDoc(y),
+                 docbook_path,
+                 doctype = doctype)
+
     pandoc_convert(
-      basename(docbook_file),
-      to = to,
+      docbook_path,
       from = "docbook",
+      to = to,
       output = normalizePath(output, winslash = "/", mustWork = FALSE),
       options = "-s"
     )
 
-    file.copy(output, file.path(wd, basename(output)), overwrite = TRUE)
+    output
+  }
 
+#' Get the text content of the abstract
+#'
+#' Gets a base R representation of the abstract in the given document, such as a
+#' character vector.
+#'
+#' This function uses Pandoc to convert the XML content contained in the
+#' abstract into Markdown and then into a base R type such a character vector or
+#' list. This is a particularly useful function to use if you want to work with
+#' the text content of your abstract to perform downstream processing. A special
+#' function like this one is required because the abstract element in EML is a
+#' TexType which supports a docbook-like structure including paragraphs,
+#' sections, titles, and much more. To work around this, the abstract XML is
+#' converted into Markdown and then read back in as a character vector and,
+#' optionally, converted to your desired type.
+#'
+#' A special case is impelemented for character vectors in that a character
+#' vector of length one is always returned.
+#'
+#' @param node An EML object or child/descendant object
+#' @param as The desired return type, see details (optional)
+#'
+#' @return A text represenation of the abstract
+#' @export
+#'
+#' @examples
+#' f <- system.file("examples/hf205.xml", package = "EML")
+#' eml <- read_eml(f)
+#' # As a single character vector
+#' get_abstract(eml, as = "character")
+#' # As a list of lines
+#' get_abstract(eml, as = "list")
+get_abstract <-
+  function(node, as = "character") {
+    # Get the abstract if an abstract wasn't passed in directly
+    if (is(node, "eml")) {
+      node <- eml_get(node, "abstract")
+    }
 
-    setwd(wd)
+    # Stop if the rest of the code isn't going to work. We either need an
+    # abstract instance or a list containing one
+    if (!(is(node, "abstract") ||
+        (is.list(node) && length(node) > 0 && is(node[[1]], "abstract")))) {
+      stop("node is not an abstract or does not contain one.", call. = FALSE)
+    }
 
-    if (view && to == "html")
-      utils::browseURL(basename(output))
+    # Run the node through Pandoc into Markdown and read it in
+    path <- node_to_file(node, "markdown")
+    abs_text <- readLines(path)
+    unlink(path)
 
+    # Tidy it up before returning it back to the user
+    if (as == "character") {
+      output <- paste(abs_text, collapse = "\n")
+    } else {
+      output <- methods::as(abs_text, as)
+    }
 
-
+    output
   }
