9c6a886b2e899d882e0073ece451cb4208da048a
Carl Boettiger <cboettig@gmail.com>
2020-07-08 21:08:25 +0000

    drop coercions

--- R/set_TextType.R
+++ R/set_TextType.R
@@ -21,22 +21,18 @@
 #' \donttest{
 #' ## using a simple character string
 #' a <- set_TextType(text = "This is the abstract")
-#' as(a, "abstract")
 #'
 #' ## Using an external markdown file
 #' f <- system.file("examples/hf205-abstract.md", package = "EML")
 #' a <- set_TextType(f)
-#' as(a, "abstract")
 #'
 #' ## Can also import from methods written in a .docx MS Word file.
 #' f <- system.file("examples/hf205-abstract.docx", package = "EML")
 #' a <- set_TextType(f)
-#' as(a, "abstract")
 #'
 #' ## Documents with title headings use `section` instead of `para` notation
 #' f <- system.file("examples/hf205-methods.docx", package = "EML")
 #' d <- set_TextType(f)
-#' as(d, "description")
 #' }
 #'
 set_TextType <- function(file = NULL, text = NULL) {
